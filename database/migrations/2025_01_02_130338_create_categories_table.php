48370d9f2a22b8c192eba54e08ea94a4302265df
VishalKumar6767 <kumarvishal27697@gmail.com>
2025-01-02 19:50:07 +0530

    adding migration of Product attacment and categories

--- database/migrations/2025_01_02_130338_create_categories_table.php
+++ database/migrations/2025_01_02_130338_create_categories_table.php
@@ -11,9 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('short_name')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
